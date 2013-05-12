a652ad139e3b5af3d8c5711d8cb4e66a0573ec75
Gregwar <g.passault@gmail.com>
2013-05-12 15:13:18 +0200

    Adding Scheduler

---
+++ src/Interpreter/Cpp/templates/Scheduler.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "Scheduler.h"
+
+using namespace std;
+
+namespace Blocks
+{
+    Scheduler::Scheduler()
+    {
+    }
+
+    Scheduler::~Scheduler()
+    {
+        map<string, SceneScheduler*>::iterator it;
+
+        for (it=scenes.begin(); it!=scenes.end(); it++) {
+            delete (*it).second;
+        }
+    }
+
+    void Scheduler::addScene(Scene *scene)
+    {
+        string name = scene->getName();
+
+        if (hasScene(name)) {
+            delete scenes[name];
+        }
+
+        scenes[name] = new SceneScheduler(scene);
+    }
+
+    bool Scheduler::hasScene(string name)
+    {
+        return scenes.find(name) != scenes.end();
+    }
+
+    vector<string> Scheduler::getScenes()
+    {
+        vector<string> sceneNames;
+        map<string, SceneScheduler*>::iterator it;
+
+        for (it=scenes.begin(); it!=scenes.end(); it++) {
+            sceneNames.push_back((*it).first);
+        }
+
+        return sceneNames;
+    }
+};
---
+++ src/Interpreter/Cpp/templates/Scheduler.h
@@ -0,0 +1,45 @@
+#ifndef _BLOCKS_SCHEDULER_H
+#define _BLOCKS_SCHEDULER_H
+
+#include <vector>
+#include "SceneScheduler.h"
+
+using namespace std;
+
+namespace Blocks
+{
+    class Scheduler
+    {
+        public:
+            Scheduler();
+            ~Scheduler();
+
+            /**
+             * Adds a scene to the scheduler
+             * 
+             * @param Scene* the scene to add
+             */
+            void addScene(Scene *scene);
+
+            /**
+             * Does the scheduler have a scene named name?
+             *
+             * @param string the scene name
+             *
+             * @return bool true if the scene is already in the scheduler
+             */
+            bool hasScene(string name);
+
+            /**
+             * Get the loaded scenes
+             *
+             * @return a vector of loaded scene names
+             */
+            vector<string> getScenes();
+
+        protected:
+            map<string, SceneScheduler*> scenes;
+    };
+};
+
+#endif
